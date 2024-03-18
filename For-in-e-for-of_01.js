707fb7bb054a0ff7ed56bdb7fbea55d49eb1ae5f
Rodorfinho021 <160938761+Rodorfinho021@users.noreply.github.com>
2024-03-17 21:11:46 -0300

    Create For-in-e-for-of_01.js
    uyouio

---
+++ For-in-e-for-of_01.js
@@ -0,0 +1,23 @@
+/*Escreva um programa que itere sobre as propriedades
+de um objeto e imprima suas chaves e valores. */
+
+import inputd from "readline-sync";
+
+let objeto = {
+    nome: "Rodolfo",
+    idade: 16,
+    escola: "IFRO Vilhena",
+    professor: "Douglas"
+};
+
+objeto.nome = inputd.question("Digite seu nome: ");
+objeto.idade =  inputd.questionInt("Digite sua idade: ");
+objeto.escola = inputd.question("Digite o nome da sua escola: ");
+objeto.professor = inputd.question("Digite o nome do seu professor: ");
+
+console.log("\n");
+
+console.log("Nome: " + objeto.nome);
+console.log("Idade: " + objeto.idade + " anos");
+console.log("Escola: " + objeto.escola);
+console.log("Professor: " + objeto.professor);
