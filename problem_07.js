a5c947a2f9fbf414b1d9897848200201ba916056
Rodorfinho021 <160938761+Rodorfinho021@users.noreply.github.com>
2024-03-28 14:53:25 -0400

    Update problem_07.js

--- problem_07.js
+++ problem_07.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
+
 import inputd from "readline-sync";
 
 let idade = inputd.question("Digite uma idade: ");
 
 
 idade >= 18 ? console.log("A pessoa é maior idade") : console.log("A pessoa é menor idade!")
-=======
-import inputd from "readline-sync";
-
-let idade = inputd.question("Digite uma idade: ");
-
-idade > 18 ? console.log("A pessoa é maior idade") : console.log("A pessoa é menor idade!")
->>>>>>> 4508e8cf946e3c8b7ab7f4bbfe3cf2ae2b6df9f8
